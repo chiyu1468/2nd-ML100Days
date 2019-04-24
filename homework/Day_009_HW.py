d234a6ca10e0629a667111253960ee326fe0ec3a
chiyu1468 <chiyu1468@hotmail.com>
2019-04-25 07:57:38 +0800

    (modify) Day09 HW
    加一些小註解

--- homework/Day_009_HW.py
+++ homework/Day_009_HW.py
@@ -3,7 +3,7 @@
 # 作業
 # - 參考範例程式碼，模擬一組負相關的資料，並計算出相關係數以及畫出 scatter plot
 # - 仿照 In[4], In[5] 的語法, 寫出負相關的變數, 並觀察相關矩陣以及分布圖
-
+# - [Pearson_correlation_coefficient](https://en.wikipedia.org/wiki/Pearson_correlation_coefficient)
 #%%
 # 載入基礎套件
 import numpy as np
@@ -18,8 +18,10 @@ get_ipython().magic('matplotlib inline')
 
 #%%
 # 隨機生成兩組 1000 個介於 0~50 的數的整數 x, y, 看看相關矩陣如何
+# noise 越高, 相關度越低
+noise = 20
 x = np.random.randint(0, 50, 1000)
-y = 25 - x + np.random.normal(0, 15, 1000)
+y = 25 - x + np.random.normal(0, noise, 1000)
 
 #%%
 # 呼叫 numpy 裡的相關矩陣函數 (corrcoef)
