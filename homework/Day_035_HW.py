da3dab2d605d15d423f89a29fe7d6f76ca6e82b3
chiyu1468 <chiyu1468@hotmail.com>
2019-05-23 14:17:34 +0800

    (add) Day 35

---
+++ homework/Day_035_HW.py
@@ -0,0 +1,20 @@
+
+#%% [markdown]
+# ## 練習時間
+# 相信大家對回歸問題及分類問題都有初步的了解。今天的作業希望大家搜尋有關 multi-label 的案例問題。下圖是電影 "奇異博士" 的分類。可以看到同時有 "Action", "Adventure", "Fantasy" 的類別存在，代表這是個多標籤的問題。 
+#%% [markdown]
+# ![image](https://cdn-images-1.medium.com/max/1000/1*r0gYXMSQf5VhdMyl2bRDyg.png)
+#%% [markdown]
+# ## 請搜尋目標為多標籤問題 (Multi-label) 的機器學習案例，了解其資料來源、目標以及評估指標為何
+# ## Hint: 服飾
+
+# ## 參考資料:
+# * [Deep dive into multi-label classification..! (With detailed Case Study)](https://towardsdatascience.com/journey-to-the-center-of-multi-label-classification-384c40229bff)
+# * [深度學習—多標籤分類問題](https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/404276/) 
+# * [多標籤分類(multi-label classification)](https://www.itread01.com/content/1543650558.html)
+# 
+# ## 一些 Kaggle 競賽:
+# * [Greek Media Monitoring Multilabel Classification (WISE 2014)](https://www.kaggle.com/c/wise-2014) 
+# * [Toxic Comment Classification Challenge](https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/overview/evaluation) 
+
+
